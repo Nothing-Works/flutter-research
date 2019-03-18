22dd3a7f5fa504c2bd884cf57f7cabc8feba9037
AndySong <andylauszp@gmail.com>
2019-03-19 00:15:53 +1300

    redo list item

--- lib/src/stateless_widget/shopping_list_item.dart
+++ lib/src/stateless_widget/shopping_list_item.dart
@@ -3,11 +3,11 @@ import 'package:flutter_research/src/models/product.dart';
 import 'package:flutter_research/src/types/function_types.dart';
 
 class ShoppingListItem extends StatelessWidget {
-  final Product product;
   final bool inCart;
+  final Product product;
   final CartChangedCallback onCartChanged;
 
-  ShoppingListItem({this.product, this.inCart, this.onCartChanged});
+  ShoppingListItem({this.inCart, this.product, this.onCartChanged});
 
   Color _getColor(BuildContext context) {
     return inCart ? Colors.black54 : Theme.of(context).primaryColor;
@@ -15,17 +15,19 @@ class ShoppingListItem extends StatelessWidget {
 
   TextStyle _getTextStyle(BuildContext context) {
     if (!inCart) return null;
+
     return TextStyle(
-        color: Colors.black54, decoration: TextDecoration.lineThrough);
+        decoration: TextDecoration.lineThrough, color: Colors.black54);
   }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap: () => onCartChanged(product, inCart),
-      leading: CircleAvatar(
-          backgroundColor: _getColor(context), child: Text(product.name[0])),
-      title: Text(product.name, style: _getTextStyle(context)),
-    );
+        onTap: () => onCartChanged(product, inCart),
+        leading: CircleAvatar(
+          backgroundColor: _getColor(context),
+          child: Text(product.name[0]),
+        ),
+        title: Text(product.name, style: _getTextStyle(context)));
   }
 }

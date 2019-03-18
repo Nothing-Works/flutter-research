4d92771c06e50880471a57765c5232cc24bdf658
AndySong <andylauszp@gmail.com>
2019-03-18 20:48:38 +1300

    scffalod

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_research/src/screens/home.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Research',
+      home: Home(),
+    );
+  }
+}
